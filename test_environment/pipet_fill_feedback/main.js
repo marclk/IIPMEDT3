7cc3385ea4794cbc71ed277cb877b15a61fe2c54
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-01-18 00:56:38 +0100

    charge bar can now decharge

--- test_environment/pipet_fill_feedback/main.js
+++ test_environment/pipet_fill_feedback/main.js
@@ -3,15 +3,16 @@ window.onload = function () {
   let lastKey = 0;
   let start;
   // let end;
-  let delta = 0;
+  let deltaG = 0;
+  let deltaH = 0;
   let visualFeedback;
   let initialBarHeight = .3;
   let modifiedDelta;
 
-  console.log("press and hold [G]");
+  console.log("press and hold [G] to rise, press and hold [H] to diminish");
 
   document.addEventListener("keydown", function(event) {
-    // keycode for [G]
+    // keycode for [G], increase
     if (event.keyCode == 71 && event.keyCode != lastKey) {
       lastKey = event.keyCode;
       console.log('keydown');
@@ -19,19 +20,41 @@ window.onload = function () {
 
       visualFeedback = setInterval(f => {
         let end = new Date();
-        delta = end - start;
-        console.log("delta: " + delta);
-        modifiedDelta = delta/2000+initialBarHeight;
+        deltaG = deltaH + (end - start);
+        console.log("deltaG: " + deltaG);
+        modifiedDelta = deltaG/2000+initialBarHeight;
         console.log("modified: " + modifiedDelta);
         bar.setAttribute("height", modifiedDelta);
         bar.setAttribute("position", "0 " + (modifiedDelta/2) + " -3");
       }, 50);
 
     }
+      // keycode for [H], decrease
+      if (event.keyCode == 72 && event.keyCode != lastKey) {
+        lastKey = event.keyCode;
+        console.log('keydown');
+        start = new Date();
+
+        visualFeedback = setInterval(f => {
+          let end = new Date();
+          deltaH =  deltaG - (end - start);
+          console.log("deltaH: " + deltaH);
+          modifiedDelta = deltaH/2000+initialBarHeight;
+          console.log("modified: " + modifiedDelta);
+          bar.setAttribute("height", modifiedDelta);
+          bar.setAttribute("position", "0 " + (modifiedDelta/2) + " -3");
+        }, 50);
+
+      }
   })
 
   document.addEventListener("keyup", function(event) {
     if (event.keyCode == 71) {
+      console.log('keyup');
+      lastKey = 0;
+      clearInterval(visualFeedback);
+    }
+    if (event.keyCode == 72) {
       console.log('keyup');
       lastKey = 0;
       clearInterval(visualFeedback);
