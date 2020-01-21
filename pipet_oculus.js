eb6a7efab1847dee89472372ed40f60afac50c88
AmosPeperkamp <s1115784@student.hsleiden.nl>
2020-01-21 12:12:20 +0100

    pipet feedback bar only activates when grabbed

--- pipet_oculus.js
+++ pipet_oculus.js
@@ -6,7 +6,7 @@ window.onload = function () {
   let start;
   let delta = 0;
   let visualFeedback;
-  let initialBarHeight = .3;
+  let initialBarHeight = 0;
   let activeBarHeight = 0;
   let modifiedDelta;
   let aButtonHeld = false;
@@ -16,27 +16,33 @@ window.onload = function () {
 
   document.querySelector('#rig').addEventListener('abuttondown', function (e) {
     aButtonHeld = true;
-    fillPipet();
+    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+      fillPipetStart();
+    }
   })
 
   document.querySelector('#rig').addEventListener('abuttonup', function (e) {
     aButtonHeld = false;
-    activeBarHeight = activeBarHeight + delta/2000;
-    clearInterval(visualFeedback);
+    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+      fillPipetEnd();
+    }
   })
 
   document.querySelector('#rig').addEventListener('bbuttondown', function (e) {
     bButtonHeld = true;
-    emptyPipet();
+    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+      emptyPipetStart();
+    }
   })
 
   document.querySelector('#rig').addEventListener('bbuttonup', function (e) {
     bButtonHeld = false;
-    activeBarHeight = activeBarHeight + delta/2000;
-    clearInterval(visualFeedback);
+    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+      emptyPipetEnd();
+    }
   })
 
-  fillPipet = () => {
+  fillPipetStart = () => {
     if (!bButtonHeld) {
       start = new Date();
 
@@ -45,12 +51,17 @@ window.onload = function () {
         delta = end - start;
         modifiedDelta = (delta/2000) + initialBarHeight + activeBarHeight;
         PIPET_FEEDBAR.setAttribute("height", modifiedDelta);
-        PIPET_FEEDBAR.setAttribute("position", "0 " + (modifiedDelta/2) + " -3");
+        PIPET_FEEDBAR.setAttribute("position", "0 " + (modifiedDelta/2) + " -.03");
       }, 50);
     }
   }
 
-  emptyPipet = () => {
+  fillPipetEnd = () => {
+    activeBarHeight = activeBarHeight + delta/2000;
+    clearInterval(visualFeedback);
+  }
+
+  emptyPipetStartStart = () => {
     if (!aButtonHeld) {
       start = new Date();
 
@@ -59,9 +70,14 @@ window.onload = function () {
         delta = -(end - start);
         modifiedDelta = (delta/2000) + initialBarHeight + activeBarHeight;
         PIPET_FEEDBAR.setAttribute("height", modifiedDelta);
-        PIPET_FEEDBAR.setAttribute("position", "0 " + (modifiedDelta/2) + " -3");
+        PIPET_FEEDBAR.setAttribute("position", "0 " + (modifiedDelta/2) + " -.03");
       }, 50);
     }
+  }
+
+  emptyPipetEnd = () => {
+    activeBarHeight = activeBarHeight + delta/2000;
+    clearInterval(visualFeedback);
   }
 
 }
