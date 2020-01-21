f702e7cf4ea11f0ea0856b2dbfbf751c320b640e
AmosPeperkamp <s1115784@student.hsleiden.nl>
2020-01-21 17:30:48 +0100

    rewrite pipet script with new eventlistener

--- pipet_oculus.js
+++ pipet_oculus.js
@@ -2,6 +2,7 @@ window.onload = function () {
   const PIPET_FEEDBAR = document.getElementById('js--pipet-feedbar');
   const PIPET = document.getElementById('js--pipet');
   const PIPET_CONTAINER = document.getElementById('js--pipet-container');
+  const GRABBABLES = document.getElementsByClassName('grabbable');
 
   let startPipetFeedbackTimer;
   let deltaPipetFeedbackTimer = 0;
@@ -12,35 +13,102 @@ window.onload = function () {
   let aButtonHeld = false;
   let bButtonHeld = false;
 
-  console.log(PIPET_CONTAINER.getAttribute("grabbable"));
+    for (let i = 0; i < GRABBABLES.length; i++) {
+      GRABBABLES[i].addEventListener('grab-start', function (e) {
 
-  document.querySelector('#rig').addEventListener('abuttondown', function (e) {
-    aButtonHeld = true;
-    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
-      fillPipetStart();
-    }
-  })
+        console.log(GRABBABLES[i].id);
 
-  document.querySelector('#rig').addEventListener('abuttonup', function (e) {
-    aButtonHeld = false;
-    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
-      fillPipetEnd();
-    }
-  })
+        let ID = GRABBABLES[i].id;
 
-  document.querySelector('#rig').addEventListener('bbuttondown', function (e) {
-    bButtonHeld = true;
-    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
-      emptyPipetStart();
-    }
-  })
+        switch (ID) {
+          case "js--pipet-container":
+            GRABBABLES[i].addEventListener('abuttondown', function (e) {
+              aButtonHeld = true;
+              fillPipetStart();
+            })
+            GRABBABLES[i].addEventListener('bbuttondown', function (e) {
+              bButtonHeld = true;
+              emptyPipetStart();
+            })
+            GRABBABLES[i].addEventListener('abuttonup', function (e) {
+              aButtonHeld = false;
+              fillPipetEnd();
+            })
+            GRABBABLES[i].addEventListener('bbuttonup', function (e) {
+              bButtonHeld = false;
+              emptyPipetEnd();
+            })
+            break;
 
-  document.querySelector('#rig').addEventListener('bbuttonup', function (e) {
-    bButtonHeld = false;
-    if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
-      emptyPipetEnd();
+          case "js--other-grabbables":
+            //FUNCTION
+            break;
+          default:
+
+        }
+      }
     }
-  })
+
+  // document.addEventListener("keydown", function(event) {
+  //   // keycode for [G], increase
+  //
+  //     for (var i = 0; i < GRABBABLES.length; i++) {
+  //
+  //       console.log(GRABBABLES[i].getAttribute("grabbed"));
+  //       console.log(GRABBABLES[i].id);
+  //
+  //       if (GRABBABLES[i].getAttribute("grabbed") == "") {
+  //         let ID = GRABBABLES[i].id;
+  //
+  //         switch (ID) {
+  //           case "js--pipet-container":
+  //             if (event.keyCode == 71 && event.keyCode != pressedKey && !hKeyHeld) {
+  //               pressedKey = event.keyCode;
+  //               gKeyHeld = true;
+  //               fillPipetStart();
+  //             } else if (event.keyCode == 72 && event.keyCode != pressedKey && !gKeyHeld) {
+  //               pressedKey = event.keyCode;
+  //               hKeyHeld = true;
+  //               emptyPipetStart();
+  //             }
+  //             break;
+  //
+  //           case "js--other-grabbables":
+  //             //FUNCTION
+  //             break;
+  //           default:
+  //
+  //         }
+  //       }
+  //     }
+
+  // document.querySelector('#rig').addEventListener('abuttondown', function (e) {
+  //   aButtonHeld = true;
+  //   if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+  //     fillPipetStart();
+  //   }
+  // })
+  //
+  // document.querySelector('#rig').addEventListener('abuttonup', function (e) {
+  //   aButtonHeld = false;
+  //   if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+  //     fillPipetEnd();
+  //   }
+  // })
+  //
+  // document.querySelector('#rig').addEventListener('bbuttondown', function (e) {
+  //   bButtonHeld = true;
+  //   if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+  //     emptyPipetStart();
+  //   }
+  // })
+  //
+  // document.querySelector('#rig').addEventListener('bbuttonup', function (e) {
+  //   bButtonHeld = false;
+  //   if (PIPET_CONTAINER.getAttribute("grabbed") == "") {
+  //     emptyPipetEnd();
+  //   }
+  // })
 
   fillPipetStart = () => {
     if (!bButtonHeld) {
