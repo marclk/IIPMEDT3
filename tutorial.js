8b40de4d97e6f499495b81e1791221dc89d623ef
AmosPeperkamp <s1115784@student.hsleiden.nl>
2020-01-21 19:23:30 +0100

    Added controller input for tutorial navigation

---
+++ tutorial.js
@@ -0,0 +1,38 @@
+let index, controlsR;
+
+window.onload = function(){
+  controlsR = document.getElementsByClassName("js--oculus-touch-controls-R");
+  controlsL = document.getElementsByClassName("js--oculus-touch-controls-L");
+
+  controlSteps = document.getElementsByClassName("js--control-step");
+
+  nextControl = () =>{
+    for (let i = 0; i < controlsR.length; i++) {
+      if(controlsR[i].hasAttribute("animation")){
+        let sourceR = controlsR[i].getAttribute("src");
+        controlsR[i].removeAttribute("animation");
+        controlsR[i].setAttribute("mtl", sourceR.replace("obj", "mtl"));
+        controlsR[i+1].setAttribute("animation", "property: components.material.material.color; type: color; from: black; to: yellow; dur: 1000; loop: true; dir: alternate; easing: easeInOutCirc");
+
+        if(i <= 1){
+          let sourceL = controlsL[i].getAttribute("src");
+          controlsL[i].removeAttribute("animation");
+          controlsL[i].setAttribute("mtl", sourceL.replace("obj", "mtl"));
+          controlsL[i+1].setAttribute("animation", "property: components.material.material.color; type: color; from: black; to: yellow; dur: 1000; loop: true; dir: alternate; easing: easeInOutCirc");
+        }else{
+          let source1 = controlsL[2].getAttribute("src");
+          controlsL[2].removeAttribute("animation");
+          controlsL[2].setAttribute("mtl", source1.replace("obj", "mtl"));
+        }
+
+        if(controlSteps[i].hasAttribute("scale")){
+          controlSteps[i].removeAttribute("scale");
+          controlSteps[i].setAttribute("color", "#dedede");
+          controlSteps[i+1].setAttribute("scale", "1.1 1.1 1.1");
+          controlSteps[i+1].setAttribute("color", "yellow");
+        }
+        return;
+      }
+    }
+  }
+}
