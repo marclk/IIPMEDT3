d0b06affae24c79e8e00c5d7f8214779a7fc13e9
AmosPeperkamp <amos.peperkamp@gmail.com>
2020-01-28 14:23:34 +0100

    implemented perdiodic table API, random element placed at "brander"

--- main.js
+++ main.js
@@ -18,6 +18,30 @@
 //       }
 //     })
 
+//==================== CHEMISTRY API STUFF
+const BASE_URL = "https://neelpatel05.pythonanywhere.com"
+const ELEMENT_NUMBER = document.getElementById('js--element-number')
+const ELEMENT_SYMBOL = document.getElementById('js--element-symbol')
+const ELEMENT_NAME = document.getElementById('js--element-name')
+const ELEMENT_MASS = document.getElementById('js--element-mass')
+
+fetchApiData = () => {
+  let randomElement = Math.floor(Math.random()* 118 + 1);
+  fetch( BASE_URL )
+  .then( (data) => {
+    return data.json();
+  })
+  .then( (response) => {
+    // AFRAME.log(response[randomElement]);
+    ELEMENT_NUMBER.setAttribute("value", response[randomElement].atomicNumber)
+    ELEMENT_SYMBOL.setAttribute("value", response[randomElement].symbol)
+    ELEMENT_NAME.setAttribute("value", response[randomElement].name)
+    ELEMENT_MASS.setAttribute("value", response[randomElement].atomicMass)
+  });
+}
+
+fetchApiData();
+
 //==================== PIPET STUFF
 const PIPET_FEEDBAR = document.getElementById('js--pipet-feedbar');
 const PIPET_CONTAINER = document.getElementById('js--pipet-container');
