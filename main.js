0f0896bd29dd6746052e1f55e4f62147ca70e6b0
AmosPeperkamp <amos.peperkamp@gmail.com>
2020-01-29 12:09:05 +0100

    Merge branch 'development' of https://github.com/marclk/IIPMEDT3 into development

--- main.js
+++ main.js
@@ -60,9 +60,25 @@ document.getElementById('cube').addEventListener('hoveron', function(evt) {
   alert("DROPPED");
 })
 
-document.getElementById('lefthand').addEventListener('thumbupstart', function(evt) {
+document.getElementById('righthand').addEventListener('thumbrightstart', function(event) {
+  // AFRAME.log("Right thumbstick");
+  let head = document.getElementById('cameraRig');
+  //let position = document.getElementById('cameraRig').getAttribute("rotation");
+  document.getElementById('cameraRig').setAttribute('rotation', {x: head.getAttribute("rotation").x,  y: (head.getAttribute("rotation").y - 45), z: head.getAttribute("rotation").z });
+});
+document.getElementById('righthand').addEventListener('thumbleftstart', function(event) {
+  // AFRAME.log("Right thumbstick");
+  let head = document.getElementById('cameraRig');
+  //let position = document.getElementById('cameraRig').getAttribute("rotation");
+  document.getElementById('cameraRig').setAttribute('rotation', {x: head.getAttribute("rotation").x,  y: (head.getAttribute("rotation").y + 45), z: head.getAttribute("rotation").z });
+});
 
-})
+// document.getElementById('righthand').addEventListener('thumbupstart', function(event) {
+//   // AFRAME.log("Right thumbstick");
+//   let head = document.getElementById('cameraRig');
+//   //let position = document.getElementById('cameraRig').getAttribute("rotation");
+//   document.getElementById('cameraRig').setAttribute('rotation', {x: head.getAttribute("rotation").x,  y: (head.getAttribute("rotation").y + 45), z: head.getAttribute("rotation").z });
+// });
 
 
 const INTERVAL_FREQ = 100;
